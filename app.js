f043d4aec49345ccc3634657c46c4012fd588484
Larry Eliemenye <denachural@gmail.com>
2015-01-09 23:14:57 +0000

    Wiring up Backend 1

--- app.js
+++ app.js
@@ -3,16 +3,27 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var session = require("express-session");
+var MongoStore = require("connect-mongo")(session);
 var bodyParser = require('body-parser');
+var flash = require("connect-flash");
+var passport = require("passport");
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//libs
+var mongoose = require("./libs/database");
+var router = require("./libs/router");
 
 var app = express();
+var sessionStore;
+mongoose.connection.once("open", function(err){
+    if(err) throw err
+    sessionStore = new MongoStore({mongoose_connection:mongoose.connection});
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,39 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
+//initialize router
+router(app)
 
 module.exports = app;
---
+++ libs/database.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose"),
+    mongooseTimestamps = require("mongoose-concrete-timestamps"),
+    autoIncrement = require("mongoose-auto-increment"),
+    config = require("../config");
+
+mongoose.connect('mongodb://localhost/'+config.database.name);
+var db = mongoose.connection;
+db.on("error",function(errMsg){
+    console.log("Error Connecting to Mongo: "+errMsg);
+});
+mongoose.set('debug', true);
+
+mongoose.plugin(mongooseTimestamps);
+autoIncrement.initialize(db);
+module.exports = mongoose;
