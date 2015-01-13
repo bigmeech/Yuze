262115c934f7d45311dc5a1e5deffd288160c32c
Larry Eliemenye <denachural@gmail.com>
2015-01-13 04:58:39 +0000

    Updated Config values for database

--- libs/database.js
+++ libs/database.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose"),
     autoIncrement = require("mongoose-auto-increment"),
     config = require("../config");
 
-mongoose.connect('mongodb://localhost/'+config.database['development'].name);
+mongoose.connect('mongodb://localhost/'+config.database['test'].name);
 var db = mongoose.connection;
 db.on("error",function(errMsg){
     console.log("Error Connecting to Mongo: "+errMsg);
