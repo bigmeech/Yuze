023ba47cfe0e34365dc64a963e2a375fa2833bfd
Larry Eliemenye <denachural@gmail.com>
2015-01-20 02:48:29 +0000

    Added Comment Upvote and Downvote operations

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -7,37 +7,39 @@ var _ = require("lodash");
 var DB = rek('database');
 
 /*
-*
-* Route Definitions
-*
-* */
+ *
+ * Route Definitions
+ *
+ * */
 
 router.get('/comments/show/:productId', showComments);
 router.put('/comments/edit/:id', editComments);
 router.post('/comments/:userId/create/:productId', createComments);
 router.delete('/comments/remove/:id', deleteComments);
+router.put('/comments/upvote/:id', uvComments);
+router.put('/comments/downvote/:id', dvComments);
 
 
 /*
-*
-* get Comments for specific product
-* Route Format get /comments/show/:productId
-*
-* */
-function showComments(req, res){
+ *
+ * get Comments for specific product
+ * Route Format get /comments/show/:productId
+ *
+ * */
+function showComments(req, res) {
     var input = req.params;
     var Comment = DB.model("Comment");
     var Product = DB.model("Product");
 
     //first attempt to find the product and proceed with the comment operation else
     // die with a 404
-    Product.findOne({productId:input.productId}, function(err, data){
-        if(err) return res.json(err, 404);
-        if(!data) return res.json({error:true, message:"Cant get comments for a product that does NOT exist"});
-        if(data){
-            Comment.find({productId:data.productId}, function(err, data){
-                if(err) return res.json(err, 404);
-                if(data.length <= 0) return res.json({error:false,message:"No Comments for this product Yet"});
+    Product.findOne({productId: input.productId}, function (err, data) {
+        if (err) return res.json(err, 404);
+        if (!data) return res.json({error: true, message: "Cant get comments for a product that does NOT exist"});
+        if (data) {
+            Comment.find({productId: data.productId}, function (err, data) {
+                if (err) return res.json(err, 404);
+                if (data.length <= 0) return res.json({error: false, message: "No Comments for this product Yet"});
                 return res.json(data);
             });
         }
@@ -47,15 +49,23 @@ function showComments(req, res){
 
 
 /*
-*
-*  You really cannot delete comments, you can only edit them
-*
-* */
-function deleteComments(req, res){
+ *
+ *  You really cannot delete comments, you can only edit them
+ *
+ * */
+function deleteComments(req, res) {
     res.send('respond with a resource');
 }
 
-function editComments(req, res){
+function editComments(req, res) {
+    res.send('respond with a resource');
+}
+
+function uvComments(req, res) {
+    res.send('respond with a resource');
+}
+
+function dvComments(req, res) {
     res.send('respond with a resource');
 }
 
@@ -71,17 +81,19 @@ function createComments(req, res) {
     // die with a 404
     Product.findOne({productId: input.productId}, function (err, data) {
         if (err) return res.json(err, 404);
-        if (!data) return res.json({
-            error: true,
-            message: "Illegal Attempt to comment on a product that does NOT exist"
-        });
+        if (!data)
+            return res.json({
+                error: true,
+                message: "Illegal Attempt to comment on a product that does NOT exist"
+            });
         if (data) {
             var NewComment = new Comment();
             NewComment.text = body.text;
             NewComment.userId = input.userId;
-            NewComment.save(function(err, data){
+            NewComment.save(function (err, data) {
                 if (err) return res.json(err, 404);
-                if (data.length <= 0) return res.json({error: false, message: "Could not find comment"});
+                if (data.length <= 0)
+                    return res.json({error: false, message: "Could not find comment"});
                 return res.json(data);
             });
         }
@@ -89,7 +101,6 @@ function createComments(req, res) {
     })
 
 }
-
 
 
 module.exports = router;
