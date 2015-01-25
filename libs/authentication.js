bd0ccab26b5da9b2f2a3a30a2d80fb731977ccbb
Larry Eliemenye <denachural@gmail.com>
2015-01-25 09:08:51 +0000

    feat:(facebook login) - Extended facebook login/signup with more scopes. Need review by facebook to work.

--- libs/authentication.js
+++ libs/authentication.js
@@ -3,21 +3,24 @@ var config = rek('config');
 var express = require('express');
 var FacebookStrategy = require("passport-facebook").Strategy;
 var passport = require("passport");
-var DB = rek("Database");
-var LocalUserModel = DB.model('User')
+var DB = rek("database");
+
+
 /*
 *
 * Passport serialization and deserializaation of user objects into session
 *
 *
 * */
-passport.serializeUser(function(user, done){
+
+ passport.serializeUser(function(user, done){
     done(null, user)
 });
 
 passport.deserializeUser(function(obj, done){
     done(null, obj)
 });
+
 /*
  *
  * Facebook related login/signup
@@ -28,23 +31,32 @@ module.exports = function (app) {
 
     /*
      *
-     * facebook strategy definition
-     * TODO: Serialize/Deserialize user into session to complete
+     * Facebook Strategy Definition
+     * --------------------------------
+     * Sign Up ----> Yuze Server-----------> go to Facebook -----> get Facebook User data ------> Create local User Account adding facebook data to it
+     * Sign IN ----> Yuze Server -----------> go to Facebook -----> get Facebook User data------> verify persisting facebook data with ours
+     *
      * */
 
     passport.use(new FacebookStrategy({
         clientID: config.keys.facebook.clientID,
         clientSecret: config.keys.facebook.clientSecret,
-        callbackURL: config.keys.facebook.callbackUrl
+        callbackURL: config.keys.facebook.callbackUrl,
+        profileFields: ['id', 'displayName', 'photos','name', 'gender','profileUrl']
     }, function (accessToken, refreshToken, profile, done) {
-        LocalUserModel.
-        console.log(arguments);
+        var LocalUserModel = DB.model('User');
         return done(null, profile);
     }));
 
+    /*
+    *
+    * facebook login url
+    * use scopes to tell passport the things you will like to retrieve from the oauth provider.
+    *
+    * */
     app.get(
         '/auth/facebook',
-        passport.authenticate('facebook'),
+        passport.authenticate('facebook',{ scope: ['user_status', 'user_likes','email','user_friends']}),
         function (req, res) {/*
             this middleware will not be used since the request has been redirected to facebook servers
         */});
